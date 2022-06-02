b972aee15c58af9da3ed3e3f74121606e0cfda8b
MateusGovea <90796629+MateusGovea@users.noreply.github.com>
2022-06-02 09:09:38 -0300

    Add files via upload

---
+++ MediaGeralPOO/MediaGeralPOO/Aluno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MediaGeralPOO
+{
+    class Aluno
+    {
+        public String Nome { get; private set; }
+        
+        private double[] _notas;
+
+        public double Media
+        {
+            get
+            {
+                return CalcularMedia();
+            }
+        }
+
+        public Aluno(String nome, int provas)
+        {
+            Nome = nome;
+            _notas = new double[provas];
+        }
+
+        public void InserirNotas() { 
+            for (int i = 0; i < _notas.Length; i++)
+            {
+                Console.Write("Nota #" + (i + 1) + ": ");
+                _notas[i] = double.Parse(Console.ReadLine());
+            }
+        }
+        private double CalcularMedia()
+        {
+            double total = 0;
+            for (int i = 0; i < _notas.Length; i++)
+            {
+                total += _notas[i];
+            }
+            return total / _notas.Length;
+        }
+    }
+}
